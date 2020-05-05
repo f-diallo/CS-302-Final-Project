dbf5f5555f868f5976efebb3d4e6152d8173854a
f-diallo <56490509+f-diallo@users.noreply.github.com>
2020-05-05 09:40:00 -0700

    Create BGL_TEST.cpp

---
+++ BGL _test/boost_1_73_0/BGL_TEST.cpp
@@ -0,0 +1,67 @@
+//Boost Graph Library test
+#include <iostream>                 // for std::cout
+#include <utility>                  // for std::pair
+#include <string>                   //for std:: string
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+
+using namespace std;
+using namespace boost;
+
+
+int main()
+{
+
+// create a typedef for the Graph type
+//typedef adjacency_list<vecS, vecS, bidirectionalS> Graph;
+typedef adjacency_list<listS, vecS, directedS, 
+                       no_property, property<edge_weight_t, int> > Graph;
+
+//example: NYC="New York City";
+string RNO="Reno", SF="San Francisco", SLC="Salt Lake City", SEA="Seattle", LV="Las Vegas";
+
+//Make labels for vertices
+const int numVertices=5;
+
+//write out edges for graph
+typedef pair<string, string> Distance;
+Distance distances[]=
+{ Distance(RNO, SLC), Distance(RNO, SEA), 
+  Distance(RNO, LV), Distance(SF, SLC), 
+  Distance(SF, SEA), Distance(SF, LV), 
+  Distance(SLC, SEA), Distance(SLC, LV), 
+  Distance(SEA, LV)};
+
+const int numEdges= sizeof(distances)/sizeof(distances[0]);
+
+//add weight/mileage to edges/distances
+int mileages[]= {520, 700, 440, 740, 800,
+                 570, 840, 420, 1100};
+
+
+//declare graph object and add edges to graph object
+Graph G(distances, distances + sizeof(distances)/sizeof(Distance), mileages, numVertices);
+
+
+//output
+
+// get the property map for vertex indices
+typedef property_map<Graph, edge_index_t>::type IndexMap;
+IndexMap index = get(edge_index, G);
+
+cout << "edges(G) =";
+graph_traits<Graph>::edge_iterator ei;
+graph_traits<Graph>::edge_iterator ei_end;
+for(tie(ei, ei_end) = edges(G); ei != ei_end; ++ei){
+    cout << "("<< index[source(*ei , G)];
+    cout << ","<< index[target(*ei , G)];
+    cout << ")"<< endl;
+
+}
+
+//7:03
+
+
+return 0;
+}
